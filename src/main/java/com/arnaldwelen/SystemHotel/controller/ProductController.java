b9fe98bbf9d3e8401b694e44fe8755568687e4f2
ArnaldWelen05 <arnaldclash@gmail.com>
2024-12-09 23:50:52 -0300

    Create insert, delete and update product

--- src/main/java/com/arnaldwelen/SystemHotel/controller/ProductController.java
+++ src/main/java/com/arnaldwelen/SystemHotel/controller/ProductController.java
@@ -1,12 +1,19 @@
 package com.arnaldwelen.SystemHotel.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arnaldwelen.SystemHotel.entites.Product;
 import com.arnaldwelen.SystemHotel.service.ProductService;
@@ -17,10 +24,35 @@ public class ProductController {
 
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll(){
+	public ResponseEntity<List<Product>> findAll() {
 		List<Product> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	 @PutMapping("/{id}")
+	    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj) {
+	        obj = service.update(id, obj);
+	        return ResponseEntity.ok().body(obj);
+	    }
 }
--- src/main/java/com/arnaldwelen/SystemHotel/service/ProductService.java
+++ src/main/java/com/arnaldwelen/SystemHotel/service/ProductService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.arnaldwelen.SystemHotel.entites.Product;
 import com.arnaldwelen.SystemHotel.repository.ProductRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class ProductService {
 
@@ -24,4 +26,31 @@ public class ProductService {
 		Optional<Product> obj = product.findById(id);
 		return obj.get();
 	}
+	public Product insert(Product obj) {
+        return product.save(obj);
+    }
+	public void delete(Long id) {
+        if (!product.existsById(id)) {
+            throw new EntityNotFoundException("Quarto não encontrado com id: " + id);
+        }
+        product.deleteById(id);
+    }
+
+    public Product update(Long id, Product obj) {
+        try {
+            Product entity = product.getReferenceById(id);
+            updateData(entity, obj);
+            return product.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Quarto não encontrado com id: " + id);
+        }
+    }
+    
+    private void updateData(Product entity, Product obj) {
+    	entity.setName(obj.getName());
+    	entity.setPrice(obj.getPrice());
+    	entity.setDescription(obj.getDescription());
+    	entity.setConsumptions(obj.getConsumptions());
+    }
+
 }
